3f68447ac448158d7dc753fabaf4b545b656017d
Rikie Patrick <rikie.patrick@drivvn.com>
2025-06-21 19:04:43 +0100

    remove dist folder and add to gitignore

--- server/dist/data/endpoints.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.endpoints = void 0;
-exports.endpoints = {
-    auth: "/api/auth/",
-    expense: "/api/expense/",
-    group: "/api/group/",
-};
